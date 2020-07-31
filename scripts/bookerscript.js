c609d46d85b1f45bce1667e7b833ac00e16a1834
raspgary <44655388+raspgary@users.noreply.github.com>
2020-07-30 22:20:09 -0500

    Update bookerscript.js

--- scripts/bookerscript.js
+++ scripts/bookerscript.js
@@ -3,7 +3,7 @@ $('.slide').hiSlide();
 $(document).ready(function () {
 
     var config = {
-    "apiKey" : "AIzaSyCm6oWzvhDOi3szra2UQja7wm19K1lsb",
+    "apiKey" : process.env.APIKEY,
     "authDomain": "americanairlines-f737a.firebaseapp.com",
     "databaseURL": "https://americanairlines-f737a.firebaseio.com/",
     "storageBucket": "americanairlines-f737a.appspot.com"
